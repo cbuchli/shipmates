e25158c5d69c2e88ea3c747786e80859e2b1462f
Christoph Buchli <cbuchli@snowflake.ch>
2015-09-09 15:55:21 +0200

    Fix Routes and Paging

--- assets/js/shipmate.js
+++ assets/js/shipmate.js
@@ -7,34 +7,22 @@
  subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-(function(document) {
-  'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at n
-  var app = document.querySelector('#app');
+displayInstalledToast = function() {
+  document.querySelector('#caching-complete').show();
+};
 
-  app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
-  };
+// See https://github.com/Polymer/polymer/issues/1381
+window.addEventListener('WebComponentsReady', function() {
+  // imports are loaded and elements have been registered
+  var template = document.querySelector('iron-pages[is="pages"]');
+  template.selected = "list";
+});
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
-  });
-
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
-
-  // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
-      drawerPanel.closeDrawer();
-    }
-  };
-})(document);
+// Close drawer after menu item is selected if drawerPanel is narrow
+onMenuSelect = function() {
+  var drawerPanel = document.querySelector('#paperDrawerPanel');
+  if (drawerPanel.narrow) {
+    drawerPanel.closeDrawer();
+  }
+};
